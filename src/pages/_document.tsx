c9eceb315045abdcef47c2af39e417644a1214ed
eddyzera <edgar.nascimento@newgo.com.br>
2022-02-23 17:55:26 -0300

    [style]: creation style global and delete file babel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,8 +12,12 @@ import { ServerStyleSheet } from 'styled-components'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="pt-br">
-        <Head></Head>
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
---
+++ src/styles/global.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  html {
+    @media (max-width: 1080px) {
+      font-size:  93.75%; // 15px
+    }
+    @media (max-width: 720px) {
+      font-size:  87.5%; // 14px
+    }
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #F7FAFC;
+    --webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+`
