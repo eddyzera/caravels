29ff30fbaded028f92397f0fa09ff6415a8418dc
eddyzera <edgar.nascimento@newgo.com.br>
2022-02-25 11:05:16 -0300

    [fix]: padding on component Header for adjust devices small

--- src/components/patterns/header/style.ts
+++ src/components/patterns/header/style.ts
@@ -2,8 +2,12 @@ import styled from "styled-components"
 
 export const Header = styled.header`
   background: ${props => props.theme.colors.white};
+
   height: 5rem;
   width: 100%;
+
+  padding: 0 2rem;
+
   display: flex;
   align-items: center;
   justify-content: space-between;
---
+++ src/infra/link/Link.tsx
@@ -0,0 +1,11 @@
+import * as NextLink from 'next/link'
+import { ReactNode } from 'react'
+
+interface LinkProps {
+  children: ReactNode
+  anchor: string
+}
+
+export const Link = ({ children, anchor }: LinkProps) => {
+  
+}
